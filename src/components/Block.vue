758ca9597cb27cd6bfe6c6da2e52792bf256a558
Saleem Abdussalam <106458354+Ethical-Saleem@users.noreply.github.com>
2022-06-20 18:33:17 +0100

    Add files via upload

---
+++ src/components/Block.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    Click Me
+  </div>
+</template>
+
+<script>
+export default {
+props: ['delay'],
+data() {
+    return {
+        showBlock: false,
+        timer: null,
+        reactTime: 0
+    }
+},
+mounted() {
+    console.log('component mounted')
+      setTimeout(() => {
+        this.showBlock = true
+        this.startTimer()
+        console.log(this.delay)
+      }, this.delay)
+    },
+methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      console.log(this.reactTime)
+      this.$emit('end', this.reactTime)
+    }
+}
+}
+</script>
+
+<style>
+  .block {
+    width: 400px;
+    border-radius: 25px;
+    background: rgba(5, 2, 24, 0.849);
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+  <p id="score"> Your Reaction Time is: {{ score }}</p>
+  <p class="rank"> {{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+        rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 180) {
+        this.rank = 'Ninja Master (Hokage)'
+    } else if (this.score < 300) {
+        this.rank = 'Ninja Fingers (Jonin)'
+    } else if (this.score < 400) {
+        this.rank = 'Rapid Reflexes (Chunin)'
+    } else if (this.score < 500) {
+        this.rank = 'Slow Fingers (Genin)'
+    } else  {
+        this.rank = 'Snail Pace...'
+    }
+  }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: rgba(5, 2, 24, 0.849);
+    font-weight: bold;
+  }
+  #score {
+    color: rgba(39, 11, 223, 0.8);
+    font-weight: bold;
+  }
+</style>
